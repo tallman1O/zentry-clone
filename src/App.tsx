1c856f9b6a191f64c18c4f3a440d5172dd48edee
Mehul Uttam <mehulsavio@gmail.com>
2024-12-04 01:51:19 +0530

    Add About component with GSAP animations and integrate into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 const App = () => {
   return (
    <main className="relative min-h-screen w-screen overflow-x-hidden">
     <Hero />
+    <About />
 
-    <section className="z-0 relative min-w-screen h-dvh bg-blue-300">hi</section>
    </main>
   )
 }
