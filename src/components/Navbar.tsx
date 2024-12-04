e1a6686dd513b257564fedd4b6a34964fc126668
Mehul Uttam <mehulsavio@gmail.com>
2024-12-04 18:30:26 +0530

    Add clsx dependency, refactor Button component for improved styling, and implement Navbar with product button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,49 @@
+import { useRef } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const navContainerRef = useRef(null);
 
-export default Navbar
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          {/* Logo and Product button */}
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10" />
+
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+
+          {/* Navigation Links and Audio Button */}
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
