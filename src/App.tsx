8410f171d9d0e9d7d8d5253af58d04df1c51bf21
Mehul Uttam <mehulsavio@gmail.com>
2024-12-06 01:21:20 +0530

    Add Features component and integrate it into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import About from "./components/About"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import About from "./components/About";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-   <main className="relative min-h-screen w-screen overflow-x-hidden">
-    <Navbar />
-    <Hero />
-    <About />
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+    </main>
+  );
+};
 
-   </main>
-  )
-}
-
-export default App
+export default App;
