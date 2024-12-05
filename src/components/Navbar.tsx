c0400a36086db4d9c7b27bbd5ed5270b661482b0
Mehul Uttam <mehulsavio@gmail.com>
2024-12-06 01:18:57 +0530

    Enhance Navbar component with scroll-based visibility and animation; add react-use dependency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useWindowScroll } from "react-use";
+import gsap from "gsap";
 
 const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
 const Navbar = () => {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
   const [isIndicatorActive, setIsIndicatorActive] = useState(false);
-  const navContainerRef = useRef(null);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [isNavVisible, setisNavVisible] = useState(true);
+
+  const navContainerRef = useRef<HTMLDivElement>(null);
   const audioElementRef = useRef<HTMLAudioElement>(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setisNavVisible(true);
+      navContainerRef.current?.classList?.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      setisNavVisible(false);
+      navContainerRef.current?.classList?.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      setisNavVisible(true);
+      navContainerRef.current?.classList?.add("floating-nav");
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
 
   const toggleAudioIndicator = () => {
     setIsAudioPlaying((prev) => !prev);
@@ -16,7 +46,7 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    if(isAudioPlaying) {
+    if (isAudioPlaying) {
       audioElementRef.current?.play();
     } else {
       audioElementRef.current?.pause();
@@ -66,15 +96,15 @@ const Navbar = () => {
                 src="/audio/loop.mp3"
                 loop
               />
-                {[1, 2, 3, 4].map((bar) => (
-                  <div
-                    key={bar}
-                    className={`indicator-line ${
-                      isIndicatorActive ? "active" : ""
-                    }`}
-                    style={{ animationDelay: `${bar * 0.1}s` }}
-                  />
-                ))}
+              {[1, 2, 3, 4].map((bar) => (
+                <div
+                  key={bar}
+                  className={`indicator-line ${
+                    isIndicatorActive ? "active" : ""
+                  }`}
+                  style={{ animationDelay: `${bar * 0.1}s` }}
+                />
+              ))}
             </button>
           </div>
         </nav>
