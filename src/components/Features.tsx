bcdebb4b5027b9aa550b5507bcf8e1dd40383533
Mehul Uttam <mehulsavio@gmail.com>
2024-12-06 21:44:50 +0530

    Adjust scale in BentoTilt component for improved visual effect

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -25,7 +25,7 @@ const BentoTilt: React.FC<{
     const tiltX = (relativeY - 0.5) * 10;
     const tiltY = (relativeX - 0.5) * -10;
 
-    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`;
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
 
     settransformStyle(newTransform);
   };
