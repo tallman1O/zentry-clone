35719c668442e8b4c8fd77c60e3d5759d241d52a
Mehul Uttam <mehulsavio@gmail.com>
2025-01-25 19:10:22 +0530

    Add video source prop to BentoCard component for dynamic content

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -47,6 +47,7 @@ const BentoTilt: React.FC<{
 };
 
 const BentoCard = ({
+  src,
   title,
   description,
 }: {
@@ -56,6 +57,13 @@ const BentoCard = ({
 }) => {
   return (
     <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
       <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
         <div>
           <h1 className="bento-title special-font">{title}</h1>
